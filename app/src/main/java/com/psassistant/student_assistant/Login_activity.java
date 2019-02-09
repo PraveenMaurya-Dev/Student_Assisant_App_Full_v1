be060ff03e8817dbfd6294b4bcb65f2f53638399
MauryaHUB <praveenmaurya404@gmail.com>
2019-02-09 14:14:50 +0530

    Register Commit, Inital 2

--- app/src/main/java/com/psassistant/student_assistant/Login_activity.java
+++ app/src/main/java/com/psassistant/student_assistant/Login_activity.java
@@ -8,6 +8,6 @@ public class Login_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_activity);
+        setContentView(R.layout.activity_login);
     }
 }
