16dbc6c463004bee91283ccc0c1731fbd688489d
MauryaHUB <praveenmaurya404@gmail.com>
2019-02-27 10:36:05 +0530

    Timetable activity phases-2 Complete , initial 2 Week-activity complete, day activity Complete

--- app/src/main/java/com/psassistant/student_assistant/WeekActivity.java
+++ app/src/main/java/com/psassistant/student_assistant/WeekActivity.java
@@ -1,10 +1,13 @@
 package com.psassistant.student_assistant;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ToolbarWidgetWrapper;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,8 +22,10 @@ import com.psassistant.student_assistant.Utils.LetterImageView;
 
 public class WeekActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private android.support.v7.widget.Toolbar toolbar;
     private ListView listView;
+    public static SharedPreferences sharedPreferences;
+    public static String SEL_DAY;
 
 
 
@@ -37,16 +42,17 @@ public class WeekActivity extends AppCompatActivity {
     }
 
     private void setupUIViews(){
-     //  toolbar = (Toolbar)findViewById(R.id.ToolbarWeek);
+       toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.ToolbarWeek);
         listView = (ListView) findViewById(R.id.lvWeek);
+        sharedPreferences = getSharedPreferences("MY_DAY", MODE_PRIVATE);
 
 
     }
 
     private void initToolbar(){
-       //setSupportActionBar(toolbar);
-       //getSupportActionBar().setTitle("Week");
-       //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       setSupportActionBar(toolbar);
+       getSupportActionBar().setTitle("Week");
+       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
@@ -65,12 +71,36 @@ public class WeekActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 switch(position){
-                    case 0:break;
-                    case 1:break;
-                    case 2:break;
-                    case 3:break;
-                    case 4:break;
-                    case 5:break;
+                    case 0:{
+                        startActivity(new Intent(WeekActivity.this, DayDetail.class));
+                        sharedPreferences.edit().putString(SEL_DAY, "Monday").apply();
+                        break;
+                    }
+                    case 1:{
+                        startActivity(new Intent(WeekActivity.this, DayDetail.class));
+                        sharedPreferences.edit().putString(SEL_DAY, "Tuesday").apply();
+                        break;
+                    }
+                    case 2:{
+                        startActivity(new Intent(WeekActivity.this, DayDetail.class));
+                        sharedPreferences.edit().putString(SEL_DAY, "Wednesday").apply();
+                        break;
+                    }
+                    case 3:{
+                        startActivity(new Intent(WeekActivity.this, DayDetail.class));
+                        sharedPreferences.edit().putString(SEL_DAY, "Thursday").apply();
+                        break;
+                    }
+                    case 4:{
+                        startActivity(new Intent(WeekActivity.this, DayDetail.class));
+                        sharedPreferences.edit().putString(SEL_DAY, "Friday").apply();
+                        break;
+                    }
+                    case 5:{
+                        startActivity(new Intent(WeekActivity.this, DayDetail.class));
+                        sharedPreferences.edit().putString(SEL_DAY, "Saturday").apply();
+                        break;
+                    }
                     default:break;
                 }
             }
