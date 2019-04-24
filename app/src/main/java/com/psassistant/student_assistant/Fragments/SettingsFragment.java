2bc2d9d83eff8e7fe3badc4d7512f89027f39f25
MauryaHUB <praveenmaurya404@gmail.com>
2019-04-24 15:59:35 +0530

    Feedback page, Chatting , about us, unresolve faculty 1

--- app/src/main/java/com/psassistant/student_assistant/Fragments/SettingsFragment.java
+++ app/src/main/java/com/psassistant/student_assistant/Fragments/SettingsFragment.java
@@ -25,7 +25,7 @@ public class SettingsFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private Button btn;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -35,6 +35,9 @@ public class SettingsFragment extends Fragment {
 
 
     public SettingsFragment() {
+
+
+
 
 
         // Required empty public constructor
